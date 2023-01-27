ac0e11e3f29407c24d345d763134267263c87b56
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2023-01-27 19:37:08 +0100

    sample surrealdb request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = reqwest::Client::new();
+
+    let res = client
+        .post("http://localhost:8000/sql")
+        .basic_auth("dan", Some("surreal"))
+        .header("Accept", "application/json")
+        .header("NS", "surreal")
+        .header("DB", "surreal")
+        .body("INFO FOR DB;")
+        .send()
+        .await?
+        .json::<Vec<SurrealResponse>>()
+        .await?;
+
+    println!("{:?}", res[0]);
+
+    Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+struct SurrealResponse {
+    time: String,
+    status: String,
+    result: serde_json::Value,
 }
