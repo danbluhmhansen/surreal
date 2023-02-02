daebf1f214360da9550fd50f133e4f0f0e81c0bd
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2023-02-02 22:04:54 +0100

    Support strings and numbers

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,7 +1,7 @@
 use convert_case::{Case, Casing};
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{Data, DeriveInput, Fields};
+use syn::{Data, DeriveInput, Fields, Type};
 
 pub trait SurrealCreate {
     fn create(&self) -> String;
@@ -14,14 +14,22 @@ pub fn impl_surreal_create(ast: DeriveInput) -> TokenStream {
         if let Fields::Named(f) = s.fields {
             let mut first: Vec<String> = vec![];
             let mut second: Vec<String> = vec![];
-            f.named
-                .into_iter()
-                .filter_map(|f| f.ident)
-                .enumerate()
-                .for_each(|(i, f)| {
-                    first.push(format!("{f} = {{{i}}}"));
-                    second.push(format!("self.{f}"));
-                });
+            f.named.into_iter().enumerate().for_each(|(i, f)| {
+                let ident = f.ident.unwrap();
+                match f.ty {
+                    Type::Path(p) => {
+                        if let Some(s) = p.path.get_ident() {
+                            if s.eq("String") {
+                                first.push(format!("{0} = '{{{1}}}'", ident, i));
+                            } else {
+                                first.push(format!("{0} = {{{1}}}", ident, i));
+                            }
+                        }
+                    }
+                    _ => {}
+                };
+                second.push(format!("self.{0}", ident));
+            });
             let first = first.join(", ");
             let second = second.join(", ");
             let snake = name.to_string().to_case(Case::Snake);
@@ -75,7 +83,7 @@ mod tests {
             quote! {
                 impl SurrealCreate for FooBar {
                     fn create(&self) -> String {
-                        format!("CREATE foo_bar SET foo = {0}, bar = {1};", self.foo, self.bar)
+                        format!("CREATE foo_bar SET foo = {0}, bar = '{1}';", self.foo, self.bar)
                     }
                 }
             }
